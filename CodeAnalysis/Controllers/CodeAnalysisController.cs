f05fa804f023d1073c3f4e7ca512667c9f60f1b8
Thoparam-Sai-nithish <thoparamsainithish1234@gmail.com>
2024-04-15 23:26:51 +0530

    first commit

---
+++ CodeAnalysis/Controllers/CodeAnalysisController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace CodeAnalysis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeAnalysisController : ControllerBase
+    {   
+        [HttpGet]
+        [Route("GetName")]
+        public string getSample()
+        {
+            return "AzureAmigos"; 
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public string ValidateData(string age, string name)
+        {
+            Console.WriteLine(age, name);
+            return "success";
+        }
+    }
+}
